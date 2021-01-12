403ad354ec97a3226ca80f88ab8fa410b6b2f46d
Dzmitri Umetsky <dima_beel@mail.ru>
2021-01-12 21:27:34 +0300

    Init

---
+++ MagicEightBallServiceLib/IEightBall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicEightBallServiceLib
+{
+    [ServiceContract]
+    interface IEightBall
+    {
+        [OperationContract]
+        string ObtainAnswerToQuestion(string userQuestion);
+    }
+}
