97b42f1ca7de3a8920dbc2a535a959a5daaf3b94
Dzmitri Umetsky <dima_beel@mail.ru>
2021-01-12 21:34:50 +0300

    Implement IEightBall

--- MagicEightBallServiceLib/MagicEightBallService.cs
+++ MagicEightBallServiceLib/MagicEightBallService.cs
@@ -7,7 +7,20 @@ using System.ServiceModel;
 
 namespace MagicEightBallServiceLib
 {
-    public class MagicEightBallService
+    public class MagicEightBallService : IEightBall
     {
+        public MagicEightBallService()
+        {
+            Console.Write("The 8-Ball is waiting for your question...");
+        }
+
+        public string ObtainAnswerToQuestion(string userQuestion)
+        {
+            var answers = new string[] { "Future Uncertain", "Yes", "No",
+            "Hazy", "Ask again later", "Definitely" };
+
+            var randomizer = new Random();
+            return answers[randomizer.Next(answers.Length)]];
+        }
     }
 }
