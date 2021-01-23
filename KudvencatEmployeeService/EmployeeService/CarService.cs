801209be1b3c18e71ca1c57ef680c84b3b359f15
Dzmitri Umetsky <dima_beel@mail.ru>
2021-01-23 23:09:32 +0300

    Rename to carservice and finish lib

--- KudvencatEmployeeService/EmployeeService/CarService.cs
+++ KudvencatEmployeeService/EmployeeService/CarService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
 
 namespace EmployeeService
 {
@@ -11,12 +7,43 @@ namespace EmployeeService
     {
         public Car GetCar(int id)
         {
-            return null;
+            using(CarContext cars = new CarContext())
+            {
+                var idParam =
+                    new System.Data.SqlClient.SqlParameter("@Id", id);
+                var foundCar =
+                    cars.Database.SqlQuery<Car>("GetCar @Id", idParam);
+                return foundCar.FirstOrDefault();
+            }
         }
 
         public void SaveCar(Car car)
         {
+            using(CarContext cars = new CarContext())
+            {
+                var parameters =
+                    new List<System.Data.SqlClient.SqlParameter>();
 
+                var idParam = new System.Data.SqlClient
+                    .SqlParameter("@Id", car.Id);
+                parameters.Add(idParam);
+
+                var markParam = new System.Data.SqlClient
+                    .SqlParameter("@Mark", car.Mark);
+                parameters.Add(markParam);
+
+                var modelParam = new System.Data.SqlClient
+                    .SqlParameter("@Model", car.Model);
+                parameters.Add(modelParam);
+
+                var YearOfProductionParam = new System.Data.SqlClient
+                    .SqlParameter("@YearOfProduction", car.YearOfProduction);
+                parameters.Add(YearOfProductionParam);
+
+                var foundCar = cars.Database
+                    .SqlQuery<Car>(
+                    "SaveCar @Id @Mark @Model @YearOfProduction", parameters);
+            }
         }
     }
 }
