350f48c51b4cb1fc3e5391e1cc984e0eafc56b7b
Dzmitri Umetsky <dima_beel@mail.ru>
2021-01-23 22:09:17 +0300

    lesson 6 start

---
+++ KudvencatEmployeeService/EmployeeService/CarService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+    public class CarService : ICarService
+    {
+        public Car GetCar(int id)
+        {
+            return null;
+        }
+
+        public void SaveCar(Car car)
+        {
+
+        }
+    }
+}
---
+++ KudvencatEmployeeService/EmployeeService/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+    [ServiceContract]
+    public interface ICarService
+    {
+        [OperationContract]
+        Car GetCar(int id);
+
+        void SaveCar(Car car);
+    }
+}
